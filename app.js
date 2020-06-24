bd087e8d8502846846e9ab30dfbf171cf6f98bd0
Ruben Dario Castillo Morales <rudacamo@gmail.com>
2020-06-24 01:23:30 +0000

    Version 1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*eslint-env node*/
 
 //------------------------------------------------------------------------------
-// hello world app is based on node.js starter application for Bluemix
+// Hola Mundo, aplicacion en node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
 // This application uses express as its web server
